6c8541b57730ba3134577e859a31ba0be09511b9
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-10-09 21:41:32 +0500

    Task1

---
+++ Tyuiu.MohnonogovaPV.Sprint2.Task1.V25.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MohnonogovaPV.Sprint2.Task1.V25.Lib
+{
+    //Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность можно
+    //чередовать, но использовать один раз в выражении)
+    //и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться),
+    //а также арифметических выражений, которая вернет
+    //логическую последовательность(массив): (True, False, True, False, True, False), при a = 247, b = 155, c = 325, d = 857
+    public class DataService : ISprint2Task1V25
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a > b) | (c < d);
+            res[1] = (a > b) & (c + 533 < d);
+            res[2] = (a > b) || (c < d);
+            res[3] = (a > b) && (c + 533 < d);
+            res[4] = !(!res[0]);
+            res[5] = (a > b) ^ (c < d);
+
+            return res;
+
+        }
+    }
+}
