fe5f7d607d911a631bfd2cc1f3f4f5f7c36f3e92
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-10-09 20:35:07 +0500

    Add project files.

---
+++ Tyuiu.MohnonogovaPV.Sprint2.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MohnonogovaPV.Sprint2.Task0.V6.Lib
+{
+    //(==, !=, <, >, <=, >=, последовательность операций не должна нарушаться) и арифметических выражений,
+    //которая вернет логическую последовательность(массив): (True, True, True, False, True, False), при x = 107, y = 754
+    public class DataService : ISprint2Task0V6
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x + 647 == y;
+            res[1] = x != y;
+            res[2] = x < y;
+            res[3] = x > y;
+            res[4] = x <= y;
+            res[5] = x >= y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MohnonogovaPV.Sprint2.Task0.V6/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.MohnonogovaPV.Sprint2.Task0.V6.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint2.Task0.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Мохноногова П. В. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                                 *");
+            Console.WriteLine("* Задание #0                                                                               *");
+            Console.WriteLine("* Вариант #6                                                                               *");
+            Console.WriteLine("* Выполнила: Мохноногова Полина Викторовна | АСОиУБ - 24-1                                 *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу из операций сравнения и арифметических выражений,                     *");
+            Console.WriteLine("* которая вернет логическую последовательность(массива).                                   *");
+            Console.WriteLine("*                                                                                          *");
+            int x = 107;
+            int y = 754;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
