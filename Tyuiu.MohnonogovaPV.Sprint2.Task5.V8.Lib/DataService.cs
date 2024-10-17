97160cab742a0cf71d2c5695140798fb3a70cc4c
Tyuiu.MohnonogovaPV@outlook.com <Tyuiu.MohnonogovaPV@outlook.com>
2024-10-17 20:17:46 +0500

    Task5

---
+++ Tyuiu.MohnonogovaPV.Sprint2.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,63 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MohnonogovaPV.Sprint2.Task5.V8.Lib
+{
+    public class DataService : ISprint2Task5V8
+    {
+        public string FindDateOfPreviousDay(int m, int n)
+        {
+            string res;
+
+            switch (n)
+            {
+                case 1:
+                    res = "01";
+                    break;
+                case 2:
+                    res = "02";
+                    break;
+                case 3:
+                    res = "03";
+                    break;
+                case 4:
+                    res = "04";
+                    break;
+                case 5:
+                    res = "05";
+                    break;
+                case 6:
+                    res = "06";
+                    break;
+                case 7:
+                    res = "07";
+                    break;
+                case 8:
+                    res = "08";
+                    break;
+                case 9:
+                    res = "09";
+                    break;
+                case 10:
+                    res = "10";
+                    break;
+                case 11:
+                    res = "11";
+                    break;
+                case 12:
+                    res = "12";
+                    break;
+                default:
+                    throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {n}");
+
+            }
+            if ((m > 1) && (m < 32))
+            {
+                m = m - 1;
+            }
+            else
+            {
+                Console.WriteLine("Число дня задано некорректно. Введите число от 2 до 31");
+            }
+            return m + "." + res;
+        }
+    }
+}
---
+++ Tyuiu.MohnonogovaPV.Sprint2.Task5.V8/Program.cs
@@ -0,0 +1,60 @@
+﻿using Tyuiu.MohnonogovaPV.Sprint2.Task5.V8.Lib;
+namespace Tyuiu.MohnonogovaPV.Sprint2.Task5.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Мохноногова П. В. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                *");
+            Console.WriteLine("* Тема: Оператор switch                                                                    *");
+            Console.WriteLine("* Задание #5                                                                               *");
+            Console.WriteLine("* Вариант #8                                                                               *");
+            Console.WriteLine("* Выполнила: Мохноногова Полина Викторовна | АСОиУБ - 24-1                                 *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,                  *");
+            Console.WriteLine("* вычисляет, и печатает результат на экране.                                               *");
+            Console.WriteLine("*                                                                                          *");
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Введите месяц:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите день: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((n < 1) || (n > 12))
+            {
+                res = "Введенно неверное значение!";
+            }
+            else
+            {
+                res = "Дата предыдущего дня - " + ds.FindDateOfPreviousDay(m, n);
+            }
+            if ((m < 1) || (m > 32))
+            {
+                res = "Введенно неверное значение!";
+            }
+            else
+            {
+                res = "Дата предыдущего дня - " + ds.FindDateOfPreviousDay(m, n);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
